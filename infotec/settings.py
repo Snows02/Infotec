c32a61d2f725244eeb324e61d71ad66352e94729
Ivan Nieves Ardila <gykour.friki.gamer@gmail.com>
2018-12-04 02:03:22 -0500

    db con postgres

--- infotec/settings.py
+++ infotec/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3%f1f$yvp(vt4)t7otw19$yt66ao&$*=fc+5)c@t)94-l!(486'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -64,11 +64,11 @@ WSGI_APPLICATION = 'infotec.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'infootec',
+         }
+ }
 
 
 # Password validation
