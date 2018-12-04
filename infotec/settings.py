01de55b16b3acddf5b78343cf7e14e48257b7c7d
Ivan Nieves Ardila <gykour.friki.gamer@gmail.com>
2018-12-04 02:09:01 -0500

    Final

--- infotec/settings.py
+++ infotec/settings.py
@@ -64,11 +64,11 @@ WSGI_APPLICATION = 'infotec.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'infootec',
-         }
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
@@ -112,6 +112,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
